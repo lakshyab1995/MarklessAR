8cb0c3fb976eaac62202d0235bba9b03d5cc21ed
lakshyab <lakshya@greedeygame.com>
2018-08-28 15:38:39 +0530

    Added Project files

---
+++ Assets/RenderScript.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderScript : MonoBehaviour {
+
+    public GameObject ufoObject;
+    public GameObject samuraiObject;
+
+    private float originalLatitude;
+    private float originalLongitude;
+    private float currentLongitude;
+    private float currentLatitude;
+
+    //private GameObject distanceTextObject;
+    private double distance;
+
+    private bool setOriginalValues = true;
+
+    private Vector3 targetPosition;
+    private Vector3 originalPosition;
+
+    private Vector3 targetSamuraiPosition;
+    private Vector3 originalSamuraiPosition;
+
+    private float speed = .1f;
+
+    IEnumerator GetCoordinates()
+    {
+        //while true so this function keeps running once started.
+        while (true)
+        {
+            // check if user has location service enabled
+            if (!Input.location.isEnabledByUser)
+                yield break;
+
+            // Start service before querying location
+            Input.location.Start(1f, .1f);
+
+            // Wait until service initializes
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+
+            // Service didn't initialize in 20 seconds
+            if (maxWait < 1)
+            {
+                print("Timed out");
+                yield break;
+            }
+
+            // Connection has failed
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                print("Unable to determine device location");
+                yield break;
+            }
+            else
+            {
+                // Access granted and location value could be retrieved
+                print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+
+                //if original value has not yet been set save coordinates of player on app start
+                if (setOriginalValues)
+                {
+                    originalLatitude = Input.location.lastData.latitude;
+                    originalLongitude = Input.location.lastData.longitude;
+                    setOriginalValues = false;
+                }
+
+                //overwrite current lat and lon everytime
+                currentLatitude = Input.location.lastData.latitude;
+                currentLongitude = Input.location.lastData.longitude;
+
+                //calculate the distance between where the player was when the app started and where they are now.
+                Calc(originalLatitude, originalLongitude, currentLatitude, currentLongitude);
+
+            }
+            Input.location.Stop();
+        }
+    }
+
+    //calculates distance between two sets of coordinates, taking into account the curvature of the earth.
+    public void Calc(float lat1, float lon1, float lat2, float lon2)
+    {
+
+        var R = 6378.137; // Radius of earth in KM
+        var dLat = lat2 * Mathf.PI / 180 - lat1 * Mathf.PI / 180;
+        var dLon = lon2 * Mathf.PI / 180 - lon1 * Mathf.PI / 180;
+        float a = Mathf.Sin(dLat / 2) * Mathf.Sin(dLat / 2) +
+          Mathf.Cos(lat1 * Mathf.PI / 180) * Mathf.Cos(lat2 * Mathf.PI / 180) *
+          Mathf.Sin(dLon / 2) * Mathf.Sin(dLon / 2);
+        var c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+        distance = R * c;
+        distance = distance * 1000f; // meters
+                                     //set the distance text on the canvas
+                                     //distanceTextObject.GetComponent<Text>().text = "Distance: " + distance;
+                                     //convert distance from double to float
+        float distanceFloat = (float)distance;
+        //set the target position of the ufo, this is where we lerp to in the update function
+        targetPosition = originalPosition - new Vector3(0, 0, distanceFloat * 12);
+        //distance was multiplied by 12 so I didn't have to walk that far to get the UFO to show up closer
+
+        targetSamuraiPosition = originalSamuraiPosition - new Vector3(0, 0, distanceFloat * 12);
+
+    }
+
+    void Start()
+    {
+        //get distance text reference
+        //distanceTextObject = GameObject.FindGameObjectWithTag("distanceText");
+        //start GetCoordinate() function 
+        StartCoroutine("GetCoordinates");
+        //initialize target and original position
+        targetPosition = ufoObject.transform.position;
+        originalPosition = ufoObject.transform.position;
+
+        targetSamuraiPosition = samuraiObject.transform.position;
+        originalSamuraiPosition = samuraiObject.transform.position;
+
+    }
+
+    void Update()
+    {
+        //linearly interpolate from current position to target position
+        ufoObject.transform.position = Vector3.Lerp(ufoObject.transform.position, targetPosition, speed);
+        //rotate by 1 degree about the y axis every frame
+        //transform.eulerAngles += new Vector3(0, 1f, 0);
+
+        samuraiObject.transform.position = Vector3.Lerp(samuraiObject.transform.position, targetSamuraiPosition, speed);
+
+    }
+}
---
+++ Assets/SoundSource.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSource : MonoBehaviour {
+
+    public GameObject samuraiObject;
+    public GameObject ufoObject;
+    AudioSource ufoAudio;
+    AudioSource samuraiAudio;
+	// Use this for initialization
+	void Start () {
+
+        ufoAudio = ufoObject.GetComponent<AudioSource>();
+        samuraiAudio = samuraiObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit");
+                if (raycastHit.collider.name == "mesh01")
+                {
+                    Debug.Log("mesh01 Hit");
+                    ufoAudio.PlayOneShot(ufoAudio.clip);
+                }
+
+                if (raycastHit.collider.name == "Plane007")
+                {
+                    Debug.Log("Plane007 Hit");
+                    samuraiAudio.PlayOneShot(samuraiAudio.clip);
+                }
+
+                //OR with Tag
+
+                //if (raycastHit.collider.CompareTag("SoccerTag"))
+                //{
+                //    
+                //}
+            }
+        }
+    }
+
+
+	private void OnMouseDown()
+	{
+        Debug.Log("Mouse Down Called");
+
+	}
+}
